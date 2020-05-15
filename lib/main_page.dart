fd1421b3d5e705474a83a2c172d203b2140fe6a7
praveen <praveen@DESKTOP-3S2412R>
2020-05-15 14:35:00 +0530

    fixed

---
+++ lib/main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class MainPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
